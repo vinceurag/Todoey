1abd2424bb50ce5386ec0959f3a3d51afb8e4015
Vince Urag <vinceuragvfx@gmail.com>
2018-12-29 01:58:18 +0800

    Convert to MVC

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Vince Urag on 29/12/2018.
+//  Copyright © 2018 Vince Urag. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
