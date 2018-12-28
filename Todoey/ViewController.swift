b234e27ca9eef9a467e2e26961ca33b328aff5ca
Vince Urag <vinceuragvfx@gmail.com>
2018-12-28 23:25:23 +0800

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Vince Urag on 28/12/2018.
+//  Copyright © 2018 Vince Urag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
